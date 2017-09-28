3c9db7c52878c8382fa295c07eda3449acdf34d0
Артем Тюменцев <artemtumencev@MacBook-Pro-Artem.local>
2017-09-28 21:21:14 +0300

    ewer

--- Calculator/AppDelegate.swift
+++ Calculator/AppDelegate.swift
@@ -60,7 +60,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         case .Sub: return arg1 - arg2
         case .Div: return arg1 / arg2
         case .Mul: return arg1 * arg2
-        }//калькулятор djkfgakdsgfkdsa
+        }//калькулятор paiben
     }
     
     @IBAction func btnNumber(sender: NSButton) {
